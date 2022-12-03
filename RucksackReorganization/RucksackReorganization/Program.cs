c2806d4d7918e7ae0d23a60bcde631c378a33440
project <yana_nacheva00@abv.bg>
2022-12-03 09:28:39 +0200

    Add Rucksack Reorganization project.

---
+++ RucksackReorganization/RucksackReorganization/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace RucksackReorganization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("Input.txt");
+            int sum = 0;
+
+            foreach (var item in lines)
+            {
+                char[] items = item.ToCharArray();
+                char[] firstPart = items.Take(items.Length / 2).ToArray();
+                char[] secondPart = items.Skip(items.Length / 2).ToArray();
+
+                char[] diff = firstPart.Intersect(secondPart).ToArray();
+                if (char.IsUpper(diff[0]))
+                {
+                    sum += (diff[0] - 38);
+                }
+                else
+                {
+                    sum += (diff[0] - 96);
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
