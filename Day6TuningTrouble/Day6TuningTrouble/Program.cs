2a238b078995f9c4b6468e35fb87c30e87681407
project <yana_nacheva00@abv.bg>
2022-12-07 00:04:11 +0200

    Add project day 6 tuning trouble

---
+++ Day6TuningTrouble/Day6TuningTrouble/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day6TuningTrouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("Input.txt");
+            int count = 0;
+            char[] letters = lines[0].ToCharArray();
+            Queue<char> queue = new Queue<char>();
+
+            foreach (var item in letters)
+            {
+                if (queue.Count == 14)
+                {
+                    break;
+                }
+                if (queue.Contains(item))
+                {
+                    while (queue.Peek() != item)
+                    {
+                        queue.Dequeue();
+                    }
+                    queue.Dequeue();
+                }
+                queue.Enqueue(item);
+                count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
